8d3d8919782133891420fba9bd030f623a30d8eb
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-01-11 19:43:21 +0000

    Test Driven Development By Example

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(amount);
+//    }
+}
