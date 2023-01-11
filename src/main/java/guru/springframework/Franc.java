48ff66d89dec22093acc0127b38cc385b17d9a01
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-01-11 20:58:03 +0000

    Test Driven Development By Example

---
+++ src/main/java/guru/springframework/Franc.java
@@ -0,0 +1,22 @@
+package guru.springframework;
+
+public class Franc {
+
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Franc franc = (Franc) o;
+        return amount == franc.amount;
+    }
+}
