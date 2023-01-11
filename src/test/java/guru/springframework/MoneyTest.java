48886f54285fb4881a52f6e8064372281967c4ab
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-01-11 22:30:50 +0000

    Test Driven Development By Example

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -37,4 +37,10 @@ public class MoneyTest {
         assertNotEquals(Money.franc(5), Money.franc(8));
         assertNotEquals(Money.franc(5), Money.dollar(5));
     }
+
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
